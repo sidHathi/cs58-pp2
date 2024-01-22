aa602373b3150bcf66fdba0eb93395808f1d26d0
sidHathi <sidhathi20@gmail.com>
2024-01-22 15:00:37 -0500

    fixed freezing bug

--- ledyard.c
+++ ledyard.c
@@ -6,10 +6,10 @@
 #include <unistd.h>
 #include <time.h>
 
-#define MAX_CARS 3
-#define MAX_TRAVEL_TIME 3
+#define MAX_CARS 4
+#define MAX_TRAVEL_TIME 6
 #define MAX_WAIT_TIME 2
-#define SIMULATION_ITERATIONS 100
+#define SIMULATION_ITERATIONS 50
 
 enum Direction {
   TO_NORWICH = 1,
@@ -38,12 +38,17 @@ typedef struct bridge_state {
 
 bridge_state_t* global_bridge_state = NULL;
 
+int waiting_on_norwich = 0;
+int waiting_on_hanover = 0;
+int waiting_on_lock = 0;
+
 static void* OneVehicle(void* dirPointer);
 static void ArriveBridge();
 static void OnBridge();
 static void ExitBridge();
 static bridge_state_t* bridge_state_new(enum Direction startDir);
 static bool bridge_state_direction_possible(bridge_state_t* bridgeState, enum Direction direction);
+static void bridge_state_signal_waiters(bridge_state_t* bridgeState);
 static void bridge_state_start_read(bridge_state_t* bridgeState);
 static void bridge_state_end_read(bridge_state_t* bridgeState);
 static void bridge_state_start_write(bridge_state_t* bridgeState);
@@ -73,7 +78,7 @@ OneVehicle(void* dirPointer)
   ArriveBridge(*castedPointer);
   OnBridge();
   ExitBridge();
-  free(dirPointer);
+  bridge_state_signal_waiters(global_bridge_state);
   return NULL;
 }
 
@@ -98,15 +103,17 @@ OnBridge()
   for ( int i = 0; i < travelIterations; i ++ ) {
     int percentageProgress = (100 * i)/travelIterations;
     bridge_state_start_read(global_bridge_state);
+    printf("OnBridge has lock\n");
 
     int numCars = global_bridge_state->num_cars;
     int currDirection = global_bridge_state->curr_direction;
 
+    bridge_state_end_read(global_bridge_state);
+
     char* directionString = currDirection == TO_HANOVER ? "Hanover" : "Norwich";
     
     printf("Car %lu is on the bridge:\n\t Traveling towards: %s \n\t Car progress: %d %% \n\t There are %d total cars on the bridge\n", currThread, directionString, percentageProgress, numCars);
-
-    bridge_state_end_read(global_bridge_state);
+    // printf("%d cars waiting on hanover, %d on norwich, %d on access lock\n", waiting_on_hanover, waiting_on_norwich, waiting_on_lock);
     sleep(1);
   }
 }
@@ -145,22 +152,24 @@ bridge_state_new(enum Direction startDir)
 bool
 bridge_state_direction_possible(bridge_state_t* bridgeState, enum Direction direction)
 {
-  bridge_state_start_read(bridgeState);
-  if (((direction == bridgeState->curr_direction) && (bridgeState->num_cars < MAX_CARS)) || bridgeState->num_cars == 0) {
-    printf("travel is possible in direction %d. Num_cars is %d\n", direction, bridgeState->num_cars);
-    bridge_state_end_read(bridgeState);
+  if (((direction == bridgeState->curr_direction) && (bridgeState->num_cars < MAX_CARS)) || bridgeState->num_cars < 1) {
+    // printf("travel is possible in direction %d. Num_cars is %d\n", direction, bridgeState->num_cars);
     return true;
   }
-  printf("travel not possible in direction %d. Num_cars is %d\n", direction, bridgeState->num_cars);
-  bridge_state_end_read(bridgeState);
+  // printf("travel not possible in direction %d. Num_cars is %d\n", direction, bridgeState->num_cars);
   return false;
 }
 
 void
 bridge_state_start_read(bridge_state_t* bridgeState)
 {
+  // printf("attempting to acquire access lock\n");
+  waiting_on_lock ++;
   pthread_mutex_lock(&bridgeState->access_lock);
+  waiting_on_lock --;
+  // printf("acquired access lock in start read\n");
   while (bridgeState->writing) {
+    // printf("waiting for write to terminate\n");
     pthread_cond_wait(&bridgeState->readable, &bridgeState->access_lock);
   }
   bridgeState->num_readers ++;
@@ -169,18 +178,24 @@ bridge_state_start_read(bridge_state_t* bridgeState)
 void
 bridge_state_end_read(bridge_state_t* bridgeState)
 {
-  pthread_mutex_unlock(&bridgeState->access_lock);
   bridgeState->num_readers --;
+  pthread_mutex_unlock(&bridgeState->access_lock);
   if (bridgeState->num_readers == 0 && !bridgeState->writing) {
     pthread_cond_signal(&bridgeState->writable);
   }
+  // printf("released access lock for read\n");
 }
 
 void
 bridge_state_start_write(bridge_state_t* bridgeState)
 {
+  // printf("attempting to acquire access lock\n");
+  waiting_on_lock ++;
   pthread_mutex_lock(&bridgeState->access_lock);
+  waiting_on_lock --;
+  // printf("acquired access lock in start write\n");
   while (bridgeState->writing || bridgeState->num_readers > 0) {
+    // printf("Waiting for read/write to terminate, numreaders: %d\n", bridgeState->num_readers);
     pthread_cond_wait(&bridgeState->writable, &bridgeState->access_lock);
   }
   bridgeState->writing = true;
@@ -190,45 +205,73 @@ void
 bridge_state_end_write(bridge_state_t* bridgeState)
 {
   // figure out which travel directions are now possible
-  pthread_mutex_unlock(&bridgeState->access_lock);
+  // printf("released access lock for write\n");
   bridgeState->writing = false;
+  pthread_mutex_unlock(&bridgeState->access_lock);
+  
+  pthread_cond_signal(&bridgeState->writable);
+  pthread_cond_broadcast(&bridgeState->readable);
+}
+
+
+void
+bridge_state_signal_waiters(bridge_state_t* bridgeState)
+{
+  bridge_state_start_write(bridgeState);
+  // printf("bridge_state_signal_waiters has lock\n");
+  bool signalHanover = false;
+  bool signalNorwich = false;
   if (bridge_state_direction_possible(bridgeState, TO_NORWICH)) {
+    // if (!bridgeState->norwich_possible) {
+    // printf("signaling in norwich direction\n");
     bridgeState->norwich_possible = true;
-    if (!bridgeState->norwich_possible) {
-      pthread_cond_signal(&bridgeState->norwich_cond);
-    }
+    signalNorwich = true;
+    // }
   } else {
     bridgeState->hanover_possible = false;
   }
   
   if (bridge_state_direction_possible(bridgeState, TO_HANOVER)) {
+    // if (!bridgeState->hanover_possible) {
+    // printf("signaling in hanover direction\n");
     bridgeState->hanover_possible = true;
-    if (!bridgeState->hanover_possible) {
-      pthread_cond_signal(&bridgeState->hanover_cond);
-    }
+    pthread_cond_broadcast(&bridgeState->hanover_cond);
+    signalHanover = true;
+    // }
   } else {
     bridgeState->hanover_possible = false;
   }
-  
-  if (bridgeState->num_readers == 0) {
-    // signal next writer
-    pthread_cond_signal(&bridgeState->writable);
+  bridge_state_end_write(bridgeState);
+  if (signalNorwich) {
+    pthread_cond_broadcast(&bridgeState->norwich_cond);
   }
-  pthread_cond_broadcast(&bridgeState->readable);
+  if (signalHanover) {
+    pthread_cond_broadcast(&bridgeState->hanover_cond);
+  }
 }
 
 bridge_state_t*
 bridge_state_add_car(bridge_state_t* bridgeState, pthread_t carThread, enum Direction direction)
 {
+  // printf("attempting to enter bridge\n");
+  pthread_mutex_lock(&bridgeState->access_lock);
   while (!bridge_state_direction_possible(bridgeState, direction)) {
     if (direction == TO_HANOVER) {
-      pthread_cond_wait(&bridgeState->hanover_cond, &bridgeState->access_lock);;
+      waiting_on_hanover ++;
+      pthread_cond_wait(&bridgeState->hanover_cond, &bridgeState->access_lock);
+      waiting_on_hanover --;
     } else {
+      waiting_on_norwich ++;
       pthread_cond_wait(&bridgeState->norwich_cond, &bridgeState->access_lock);
+      waiting_on_norwich --;
     }
   }
+  pthread_mutex_unlock(&bridgeState->access_lock);
 
+  // printf("attempting to write\n");
   bridge_state_start_write(bridgeState);
+
+  // printf("bridge_state_add_car has lock\n");
   
   int numCars = bridgeState->num_cars;
   bridgeState->cars[numCars] = carThread;
@@ -236,6 +279,7 @@ bridge_state_add_car(bridge_state_t* bridgeState, pthread_t carThread, enum Dire
   bridgeState->curr_direction = direction;
 
   bridge_state_end_write(bridgeState);
+  // printf("finished writing\n");
   return bridgeState;
 }
 
@@ -243,6 +287,7 @@ bridge_state_t*
 bridge_state_remove_car(bridge_state_t* bridgeState, pthread_t carThread)
 {
   bridge_state_start_write(bridgeState);
+  // printf("bridge_state_remove_car has lock\n");
   int numCars = bridgeState->num_cars;
   int carIndex = 0;
   for ( int i = 0; i < numCars; i ++ ) {
@@ -261,6 +306,7 @@ bridge_state_remove_car(bridge_state_t* bridgeState, pthread_t carThread)
   bridgeState->num_cars --;
 
   bridge_state_end_write(bridgeState);
+  bridge_state_signal_waiters(bridgeState);
   return bridgeState;
 }
 
@@ -299,6 +345,7 @@ simulate_traffic()
   for ( int i = 0; i < SIMULATION_ITERATIONS; i ++ ) {
     pthread_join(threads[i], NULL);
   }
+  printf("Simulation complete\n");
 }
 
 pthread_t
@@ -306,7 +353,7 @@ start_car_thread(enum Direction direction)
 {
   pthread_t thread_id;
   enum Direction* dirPointer = malloc(sizeof(enum Direction));
-  *dirPointer = direction;
+  *dirPointer = direction; // I don't know how to safely free this memory
 
   if (pthread_create(&thread_id, NULL, OneVehicle, (void*)dirPointer) != 0) {
     printf("Error in thread creation for %lu\n", thread_id);
